1420f5214b432a5fc142a219db7814d94fe6ed9c
林周 <970060731@qq.com>
2023-07-13 11:48:13 +0800

    init 持续更新工具类

--- kylin-lock/src/main/java/com/kylin/lock/aspect/LockAspect.java
+++ kylin-lock/src/main/java/com/kylin/lock/aspect/LockAspect.java
@@ -40,29 +40,29 @@ public class LockAspect {
             return joinPoint.proceed();
         }
         //获取分布式锁的key
-        List<String> redisKey = getRedisKey(joinPoint, lock);
-        if (CollectionUtils.isEmpty(redisKey)) {
-            log.error("未获取到分布式锁key,跳过加锁逻辑，redisKey：{}", redisKey);
+        List<String> redisKeyList = getRedisKey(joinPoint, lock);
+        if (CollectionUtils.isEmpty(redisKeyList)) {
+            log.error("未获取到分布式锁key,跳过加锁逻辑，redisKey：{}", redisKeyList);
             return joinPoint.proceed();
         }
 
 
         DistributedLock distributedLock = applicationContext.getBean(lock.lockType());
 
-        LockContext lockContext = new LockContext(redisKey, lock.waitLockTime());
+        LockContext lockContext = new LockContext(redisKeyList, lock.waitLockTime());
         BaseLockResult lockResultBo = distributedLock.lock(lockContext);
         try {
             if (Objects.isNull(lockResultBo)) {
-                log.error("分布式锁获取失败，redisKey：{}", redisKey);
+                log.error("分布式锁获取失败，redisKey：{}", redisKeyList);
                 throw new BizException(LockErrorResultCodeEnums.FREQUENT);
             } else {
-                log.info("分布式锁获取成功，redisKey：{}", redisKey);
+                log.info("分布式锁获取成功，redisKey：{}", redisKeyList);
             }
             return joinPoint.proceed();
         } finally {
             if (Objects.nonNull(lockResultBo)) {
                 distributedLock.unLock(lockResultBo);
-                log.info("分布式锁释放成功，redisKey：{}", redisKey);
+                log.info("分布式锁释放成功，redisKey：{}", redisKeyList);
             }
         }
     }
